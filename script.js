b3c9489751d133984378509efafab83d003d1896
Daniel Vazquez <dvazquez21@berkeley.edu>
2021-09-01 07:29:22 -0700

    flip and match work

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const cardsArray = [1,1,2,2,3,3,4,4,5,5,6,6]
 
 const board = document.querySelector('.flex-container')
@@ -5,6 +6,7 @@ const board = document.querySelector('.flex-container')
 var selected = []
 
 cardsArray.sort(() => 0.5 - Math.random())
+
 
 function resetBoard(){
     board.innerHTML = ''
@@ -16,8 +18,9 @@ function createBoard(){
     board.innerHTML = ''
     for(let i = 0; i < cardsArray.length; i++ ){
         var card = document.createElement('div')
-        // card.setAttribute()
-        card.addEventListener("click", flip());
+        card.classList.add('card')
+        card.setAttribute('id', i)
+        card.addEventListener("click", flip);
         // card.onclick() = function(){flip()}
         card.innerHTML = cardsArray[i];
         card.style.width = '400px';
@@ -28,17 +31,31 @@ function createBoard(){
 }
 
 function flip(){
-    console.log('test');
+    var card_id = this.getAttribute('id')
+    selected.push(card_id)
     // selected.push(position)
     if(selected.length === 2){
         console.log('test');
-        setTimeout(matched(), 600)
+        setTimeout(matched, 600)
     }
 
 }
 
 function matched(){
     console.log('clicked');
+    var cards = document.getElementsByClassName('card')
+    var first_id = selected[0]
+    var second_id = selected[1]
+    var first_card = cardsArray[first_id]
+    var second_card = cardsArray[second_id]
+    if(first_card === second_card && first_id != second_id){
+        alert('Match found!')
+        cards[first_id].innerHTML = 'matched'
+        cards[second_id].innerHTML = 'matched'
+    }else{
+        alert('No match')
+    }
+    selected = []
 }
 
 // createBoard()

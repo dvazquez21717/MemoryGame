f2c95ebd5b20d67a12593e74b1937eb11ff6f216
Daniel Vazquez <dvazquez21@berkeley.edu>
2021-09-04 15:58:21 -0700

    Wenhao Zhao's JS additions (try counter)

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ const cardsArray = [
 
 const board = document.querySelector('.flex-container')
 
-var selected = []
+var selected = [];
+var clicks = 0;
+var scores = 0;
 
 cardsArray.sort(() => 0.5 - Math.random())
 
@@ -24,25 +26,26 @@ cardsArray.sort(() => 0.5 - Math.random())
 // from stackoverflow to reset page 
 function resetBoard(){
    window.location.reload();
+   clicks = 0;
 }
 
 function createBoard(){
     board.innerHTML = ''
     for(let i = 0; i < cardsArray.length; i++ ){
-        var parent = document.createElement('div')
-        var card = document.createElement('img')
-        parent.classList.add('parent')
-        card.classList.add('card')
-        card.setAttribute('id', i)
+        var parent = document.createElement('div');
+        var card = document.createElement('img');
+        parent.classList.add('parent');
+        card.classList.add('card');
+        card.setAttribute('id', i);
         card.setAttribute('src', 'images/questionmark.png')
         card.addEventListener("click", flip);
         card.innerHTML = cardsArray[i];
-        parent.appendChild(card)
-        board.appendChild(parent)
+        parent.appendChild(card);
+        board.appendChild(parent);
 
-        
+
     }
-    var btn = document.getElementsByClassName('reset')[0];
+    var btn = document.getElementsByClassName("reset")[0];
     btn.style.display = 'flex';
 
 }
@@ -54,6 +57,7 @@ function flip(){
     // selected.push(position)
     if(selected.length === 2){
         setTimeout(matched, 500)
+        clicks++;
     }
 
 }
@@ -65,14 +69,19 @@ function matched(){
     var first_card = cardsArray[first_id].name
     var second_card = cardsArray[second_id].name
     if(first_card === second_card && first_id != second_id){
-        alert('Match found!')
-        
+        alert('Match found!'+ " current try is: "+ clicks );
+        ++scores;
+        if(scores === cardsArray.length/2){
+            alert("your total tries are: " + clicks);
+            resetBoard();
+        }
+
     }else{
-        alert('No match')
+        alert('No match,'+ " current try is: "+ clicks )
         cards[first_id].setAttribute('src', 'images/questionmark.png')
         cards[second_id].setAttribute('src', 'images/questionmark.png')
     }
     selected = []
 }
 
-// createBoard()
+// createBoard() 
